dc323346c8695ca797dadb28de5aa3247a887902
Nileshh03 <nilesh.singh@kickdrumtech.com>
2026-02-10 20:10:02 +0530

    Small change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // Health check endpoint (VERY IMPORTANT for EB)
 app.get("/health", (req, res) => {
     console.log("helo");
-  res.status(200).json({ status: "UP" });
+  res.status(200).json({ status: "running api" });
 });
 
 // Sample API endpoint
