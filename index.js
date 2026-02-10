8ceac5bf6314572eaf12833f8f77979d726116a5
Nileshh03 <nilesh.singh@kickdrumtech.com>
2026-02-10 15:53:51 +0530

    Initial Node.js backend API

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// Health check endpoint (VERY IMPORTANT for EB)
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+// Sample API endpoint
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from AWS Elastic Beanstalk!" });
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
